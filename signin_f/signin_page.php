0e21c247fc40dca199419741e1dca823223ac25a
mauricio.rodriguez <maurirodriguezm48@gmail.com>
2024-12-17 13:55:21 -0600

    finish signin page and gave it style

--- signin_f/signin_page.php
+++ signin_f/signin_page.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signin</title>
+    <link rel="stylesheet" href="signin_style.css">
 </head>
 <body>
     <div class="signin-wrapper">
-        <div class="signin-wrap">
-            <h1>Sign In</h1>
+        <div>
+            <h1 class="logo">Maubook</h1>
+            <div class="signin-wrap">
+                <h1 style="text-align: center;">Sign In</h1>
+                <p style="text-align: center; font-weight:400;">create your Maubook account!</p>
+                <form action="" method="POST">
+                    <div class="form-wrap">
+                        <input type="text" name="username" placeholder="Username">
+                        <input type="text" name="email" placeholder="E-mail ">
+                        <input type="password" name="pwd" placeholder="Password">
+                        <div class="signin-btn-wrapper">
+                            <button type="submit" class="signin-btn">Signin</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
