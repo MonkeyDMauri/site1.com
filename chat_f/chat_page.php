b4bd86dea9581d730b69a2b6d49d4f43644007fe
mauricio.rodriguez <maurirodriguezm48@gmail.com>
2024-12-21 23:51:57 -0600

    create template for chat UI

--- chat_f/chat_page.php
+++ chat_f/chat_page.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Room</title>
+    <title>My Chat</title>
+    <link rel="stylesheet" href="./chat_style.css">
 </head>
 <body>
-    <h1>chat room</h1>
+    <div class="wrapper">
+        <div class="left-pannel">
+        </div>
+        <div class="right-pannel">
+            <div class="header">My Chat</div>
+            <div class="containe">
+                <div class="inner-left-pannel"></div>
+                <div class="inner-right-pannel"></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
