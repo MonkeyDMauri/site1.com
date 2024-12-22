554251468187458ebf6a309494c6e4f1f6e3a89d
mauricio.rodriguez <maurirodriguezm48@gmail.com>
2024-12-22 15:28:58 -0600

    work on 'left-pannel' chat frontend

--- chat_f/chat_page.php
+++ chat_f/chat_page.php
@@ -9,11 +9,32 @@
 <body>
     <div class="wrapper">
         <div class="left-pannel">
+            <div style="padding:1rem;">
+                <img class="profile-img" src="./chat_pics/ui/images/user3.jpg" alt="user 3">
+                <br>
+                Kelly Hoftstader
+                <br>
+                <div style="opacity:0.5;">Kelly.hoftstader@gmail.com</div>
+                <br><br><br>
+                <div>
+                    <label for="box">Chat
+                        <img src="./chat_pics/ui/icons/chat.png" alt="">
+                    </label>
+                    <label for="">Contacts
+                        <img src="./chat_pics/ui/icons/contacts.png" alt="">
+                    </label>
+                    <label for="">Settings
+                        <img src="./chat_pics/ui/icons/settings.png" alt="">
+                    </label>
+                </div>
+            </div>
         </div>
         <div class="right-pannel">
             <div class="header">My Chat</div>
             <div class="containe">
-                <div class="inner-left-pannel"></div>
+                <div class="inner-left-pannel">
+                    <input type="checkbox" id="box" name="">
+                </div>
                 <div class="inner-right-pannel"></div>
             </div>
         </div>
